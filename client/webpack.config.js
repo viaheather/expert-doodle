2625994e875c54d2c318988eaf0434f388aab88e
viaheather <viaheatherr@gmail.com>
2023-12-13 21:49:35 -0800

    updated file structure and webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,8 +17,36 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
-    plugins: [],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
 
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "JATE - The Text Editor!",
+        background_color: "#132e4f",
+        theme_color: "#132e4f",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
     module: {
       // CSS loaders
       rules: [
